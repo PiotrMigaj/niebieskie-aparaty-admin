ba421775d55811a84d79bd945bcb0218667aeaac
pmigaj <pm@nitzinger-it.com>
2025-04-04 12:39:38 +0200

    little lint fix

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -12,7 +12,7 @@ const JWT_EXPIRES_IN: string = process.env.JWT_EXPIRES_IN || '7d';
 // Generate JWT token
 export const generateToken = (id: string, username: string): string => {
   // Ensure you're passing the correct type for `JWT_SECRET`
-  // @ts-ignore - Bypass the TypeScript error for expiresIn
+  // @ts-expect-error - Bypass the TypeScript error for expiresIn
   return jwt.sign(
     { id, username }, // Payload
     JWT_SECRET, // Secret or private key (must be a valid key)
